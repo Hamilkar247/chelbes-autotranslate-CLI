42c933fdef69dab83562c25b68ef9d84cb439f2a
Hamilkar247 <adonnibal96@gmail.com>
2020-11-05 21:49:50 +0100

    ISS-1 develop chelbes_autotranslate.py - użycie biblioteki argparse i logging

--- chelbes_translator.py
+++ chelbes_translator.py
@@ -1,26 +1,48 @@
 import googletrans
 import os
 import sys
+import argparse
+import logging
 
 
-def translateChelbes(translator: googletrans.Translator, only_text: str, lang_from: str, lang_to: str) -> str:
-    resultOfTrans = translator.translate(only_text, src=lang_from, dest=lang_to)
+def def_params():
+    parser = argparse.ArgumentParser(
+            description=
+"""
+Script to translate sentence with google
+EXAMPLE: python chelbes_translator -f pl -to eng -s \"polska dla polaków\" -l
+"""
+    )
+    #action="store_true" sprawia że potem już nie musze podawać liczby/stringa do tego argumentu
+    parser.add_argument("-l", "--loghami", action="store_true", help="set debug", required=False)
+    parser.add_argument("-f", "--fromlang", help="source language sentence", required=True)
+    parser.add_argument("-t", "--tolang", help="destination language sentence", required=True)
+    parser.add_argument("-s", "--sentence", help="sentence to translate", required=True)
+    args = parser.parse_args()
+    if args.loghami:
+        logging.basicConfig(level=logging.DEBUG)
+        print("args:" + str(args))
+    return args
+
+
+def translateChelbes(translator, sentence, lang_from, lang_to):
+    resultOfTrans = translator.translate(sentence, src=lang_from, dest=lang_to)
     return resultOfTrans.text
 
 
 def main():
-    #print('Number of arguments:', len(sys.argv), ' arguments.')
-    #print(googletrans.LANGUAGES.keys())
-    sentence = sys.argv[3:]
-    lang_from = sys.argv[1]
-    lang_to = sys.argv[2]
-    result=" ".join(sentence)
+    args=def_params()
+    logging.debug('Only shown in debug mode')
+    logging.debug("args.sentence: "+args.sentence)
+    logging.debug("args.fromlang: "+args.fromlang)
+    sentence = args.sentence
+    lang_from = args.fromlang
+    lang_to = args.tolang
     translator = googletrans.Translator()
-    translate_text = translateChelbes(translator, result, lang_from, lang_to)
+    translate_text = translateChelbes(translator, sentence, lang_from, lang_to)
     print(translate_text)
 
 
 if __name__ == "__main__":
     main()
-
 
